c9437e75dae7057fa0921519dd5aca93eb2397ce
吕海振 <lvhaizhen@lvhaizhendeMacBook-Pro.local>
2018-07-24 22:44:06 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+#read-api
+ 提供restful api接口服务
+
+#read-backend
+ 基于vue+es6的后台管理系统
+
+#read-smallApp
+ 小程序
+
+#read-web
+  h5 web服务，支持seo.
---
+++ read-web/src/main/java/com/sea/ReadWebApplication.java
@@ -0,0 +1,12 @@
+package com.sea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadWebApplication.class, args);
+	}
+}
